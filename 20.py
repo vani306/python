49ce3074b0cf1d4e952fe02a8c5348579dd5ac0c
vani306 <24BIT031@sot.pdpu.ac.in>
2025-01-16 16:39:40 +0530

    Add files via upload

---
+++ 20.py
@@ -0,0 +1,6 @@
+#//calculate area of triangle
+height=int(input("enter height of triangle"))
+length=int(input("enter length of triangle"))
+area=1/2*height*length
+print("area of triangle is",area)
+
