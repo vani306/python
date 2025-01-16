cfe5b2ec91e3d9355dc63c26673d7880de5ed0c1
vani306 <24BIT031@sot.pdpu.ac.in>
2025-01-16 16:35:08 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,7 @@
+#//add, subtract, multiply, divide two numbers
+a=int(input("first number is"))
+b=int(input("second number is"))
+print("addition is", a+b)
+print("subtraction is", a-b)
+print("multiplication is", a*b)
+print("division is", a/b)
