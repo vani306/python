9fb872d08a81cb67787f795608d038ac74ed5fe4
vani306 <24BIT031@sot.pdpu.ac.in>
2025-01-16 16:38:47 +0530

    Add files via upload

---
+++ 17.py
@@ -0,0 +1,6 @@
+#//calculate area and perimeter of square
+length=int(input("enter length of a square"))
+area=length*length
+perimeter=4*length
+print("area of square is",area)
+print("perimeter of square is",perimeter)
