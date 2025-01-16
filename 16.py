79b6d779ba63bc478c953f63c1f0b01a537de8bf
vani306 <24BIT031@sot.pdpu.ac.in>
2025-01-16 16:38:16 +0530

    Add files via upload

---
+++ 16.py
@@ -0,0 +1,6 @@
+#//calculate interest
+principal=int(input("enter a value"))
+number=int(input("enter a value"))
+rate=int(input("enter a value"))
+interest=(principal*number*rate)/100
+print(interest)
