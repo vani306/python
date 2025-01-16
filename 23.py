9b94c9f8581ad33604ee6b8dfcc0270fa355745c
vani306 <24BIT031@sot.pdpu.ac.in>
2025-01-16 20:19:58 +0530

    Add files via upload

---
+++ 23.py
@@ -0,0 +1,8 @@
+#//calculate average of three subjects along with their total
+a=int(input("enter marks of maths"))
+b=int(input("enter marks of biology"))
+c=int(input("enter marks of chemistry"))
+total_marks=a+b+c
+average=(a+b+c)/3
+print(total_marks)
+print(average)
