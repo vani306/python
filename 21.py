cca824dbb9d979125dbb88860ed72c5091a78cc5
vani306 <24BIT031@sot.pdpu.ac.in>
2025-01-20 19:28:21 +0530

    Add files via upload

---
+++ 21.py
@@ -0,0 +1,6 @@
+#//calculate net salary
+gross_salary=float(input("enter the gross_salary"))
+allowance=(10/100)*gross_salary
+deduction=(3/100)*gross_salary
+net_salary=gross_salary+allowance-deduction
+print(net_salary)
