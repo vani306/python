08c7e8a1710f788f010bc10f324fb8b3f8a3b516
vani306 <24BIT031@sot.pdpu.ac.in>
2025-01-21 15:34:17 +0530

    Add files via upload

---
+++ lab2 - 3.py
@@ -0,0 +1,12 @@
+#//check if a given number is odd or even
+a=int(input("enter first value"))
+b=int(input("enter second value"))
+if (a%2)==0:
+    print("even value is", a)
+else :
+    print("a is odd")
+if(b%2)==0:
+    print("even value is", b)
+else :
+    print("b is odd")
+
