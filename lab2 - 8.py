d56f6b40811658a87dc028a7cfcaa5b94f1f5868
vani306 <24BIT031@sot.pdpu.ac.in>
2025-01-21 15:54:50 +0530

    Add files via upload

---
+++ lab2 - 8.py
@@ -0,0 +1,8 @@
+#//check if triangle is valid or not
+a=int(input("enter first angle"))
+b=int(input("enter second angle"))
+c=int(input("enter third angle"))
+if (a+b+c)==180:
+    print("triangle is valid")
+else :
+    print("triangle is not valid")
