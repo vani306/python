be06131d8fe9c0d68c67aa9fa466234f26f3d134
vani306 <24BIT031@sot.pdpu.ac.in>
2025-01-21 15:34:43 +0530

    Add files via upload

---
+++ lab2 - 4.py
@@ -0,0 +1,11 @@
+#//check i a given nu ber is divisible by 10 or not
+a=int(input("enter a value"))
+b=int(input("enter a value"))
+if a%10==0:
+    print(" a is divisible by 10")
+else :
+    print(" a is not divisible by 10")
+if  b%10==0:
+    print("b is divisible by 10")
+else :
+    print("b is not divisible by 10")
