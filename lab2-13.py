500573b4a8aff8f4a171465312677d2e9a2f4dde
vani306 <24BIT031@sot.pdpu.ac.in>
2025-02-04 08:07:59 +0530

    Add files via upload

---
+++ lab2-13.py
@@ -0,0 +1,43 @@
+#//convert number_to_words 
+def number_to_words(n):
+    if num==1:
+        print("one")
+    if num==2:
+        print("two")
+    if num==3:
+        print("three")
+    if num==4:
+        print("four")
+    if num==5:
+        print("five")
+    if num==6:
+        print("six")
+    if num==7:
+        print("seven")
+    if num==8:
+        print("eight")
+    if num==9:
+        print("nine")
+    if num==10:
+        print("ten")
+    if num==11:
+        print("eleven")
+    if num==12:
+        print("twelve")
+    if num==13:
+        print("thirteen")
+    if num==14:
+        print("fourteen")
+    if num==15:
+        print("fifteen")
+    if num==16:
+        print("sixteen")
+    if num==17:
+        print("seventeen")
+    if num==18:
+        print("eighteen")
+    if num==19:
+        print("nineteen")
+    else:
+        print("number out of range")
+                                                            
