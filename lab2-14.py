079ba96db7f894ab520a4c91f877a2eec359c055
vani306 <24BIT031@sot.pdpu.ac.in>
2025-02-04 08:08:17 +0530

    Add files via upload

---
+++ lab2-14.py
@@ -0,0 +1,24 @@
+marks1=int(input("enter marks of maths"))
+marks2=int(input("enter marks of biology"))
+marks3=int(input("enter marks of chemistry"))
+total_marks=marks1+marks2+marks3
+average_marks=(marks1+marks2+marks3)/3
+print(total)
+print(average)
+if marks<=39:
+   print("grade is F")
+if marks>=40 & marks<=44:
+   print("grade is P")
+if marks>=45 & marks<=49:
+   print("grade is C")
+if marks>=50 & marks<=54:
+   print("grade is B")
+if marks>=55 & marks<=59:
+   print("grade is B+")
+if marks>=60 & marks<=69:
+   print("grade is A")
+if marks>=70 & marks<=79:
+   print("grade is A+")
+if marks>=80 & marks<=100:
+   print("grade is F")
+ 
