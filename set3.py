eda85abf4ddf2f41a456492757f36bd6b8976d4d
vani306 <24BIT031@sot.pdpu.ac.in>
2025-02-25 15:39:03 +0530

    Add files via upload

---
+++ set3.py
@@ -0,0 +1,15 @@
+a= set()
+
+a.add("vani")
+a.add("kabir")
+a.add("avani")
+a.add("vini")
+a.add("prachi")
+
+a.remove("vini")
+a.add("vinit")
+
+a.remove("prachi")
+a.remove("kabir")
+
+print("set of names: ", a) 
