d95516bf41d39dffc82c357575b6d22495e4a86a
vani306 <24BIT031@sot.pdpu.ac.in>
2025-02-25 13:37:45 +0530

    Add files via upload

---
+++ lab4-3.py
@@ -0,0 +1,8 @@
+
+str1 = input("Enter the first string: ")
+str2 = input("Enter the second string: ")
+
+if str2 in str1:
+    print("Yes, the second string is found in the first string.")
+else:
+    print("No, the second string is not found in the first string.")
