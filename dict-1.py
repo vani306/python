6f365f1383530b6008e4dd43cf5d3e7d3730f2f2
vani306 <24BIT031@sot.pdpu.ac.in>
2025-03-12 23:05:51 +0530

    Add files via upload

---
+++ dict-1.py
@@ -0,0 +1,9 @@
+
+dict1 = {1: "tulip", 2: "rose"}
+dict2 = {3: "lily", 4: "daisy"}
+dict3 = {5: "sunflower", 6: "orchid"}
+
+final_dict = dict1 | dict2 | dict3
+
+print("Concatenated Dictionary:", final_dict)
+
