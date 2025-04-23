b846882c460c5f3850d426ff9104372e446bc74c
vani306 <24BIT031@sot.pdpu.ac.in>
2025-04-23 14:57:38 +0530

    Add files via upload

---
+++ exception handling.py
@@ -0,0 +1,7 @@
+while True:
+    try:
+        num = int(input("Enter an integer: "))
+        print("You entered:", num)
+        break
+    except ValueError:
+        print("Invalid input! Please enter an integer.")
